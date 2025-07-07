955bc6da4f26c6f9703a50a25040603e595ddbbb
orthosquare-marketing <marketing@orthosquare.com>
2025-07-07 07:53:37 +0000

    update bottum footer

--- src/components/Mobile_bottum.tsx
+++ src/components/Mobile_bottum.tsx
@@ -1,12 +1,24 @@
-const Mobile_bottum = () => {
-    return(
-<div className="block lg:hidden container ">
-        <div className="grid grid-cols-2 gap-4">
-            <div className="text-white bg-[#1280b5] p-3"> Enquiry Now</div>
-            <div className="text-white bg-[#000000] p-3">Call Now</div>
-        </div>
-</div>
-    )
+const MobileBottom = () => {
+  return (
+    <div className="fixed bottom-0 left-0 right-0 block lg:hidden z-50">
+      <div className="grid grid-cols-2 gap-0">
+        <a
+          href="https://wa.me/919167195818"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-white text-center bg-[#1280b5] p-3 font-semibold"
+        >
+          Enquiry Now
+        </a>
+        <a
+          href="tel:919167195818"
+          className="text-white text-center bg-black p-3 font-semibold"
+        >
+          Call Now
+        </a>
+      </div>
+    </div>
+  );
 };
 
-export default Mobile_bottum
+export default MobileBottom;
