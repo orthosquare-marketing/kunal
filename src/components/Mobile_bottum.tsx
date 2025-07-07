6759ff6202c5f6bb6393872eda406a7e1fc3238f
orthosquare-marketing <marketing@orthosquare.com>
2025-07-07 08:53:39 +0000

    bottun call

--- src/components/Mobile_bottum.tsx
+++ src/components/Mobile_bottum.tsx
@@ -1,18 +1,21 @@
 const MobileBottom = () => {
   return (
-    <div className="fixed bottom-0 left-0 right-0 block lg:hidden z-50">
+    <div
+      className="fixed bottom-0 left-0 right-0 block lg:hidden z-50"
+      style={{ paddingBottom: 'env(safe-area-inset-bottom)' }} // Key Fix
+    >
       <div className="grid grid-cols-2 gap-0">
         <a
           href="https://wa.me/919167195818"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-white text-center  bg-[#ff7f50] p-3 font-semibold"
+          className="text-white text-center bg-[#ff7f50] p-3 font-bold text-lg"
         >
           Enquiry Now
         </a>
         <a
           href="tel:919167195818"
-          className="text-white text-center bg-[#1280b5] p-3 font-semibold"
+          className="text-white text-center bg-[#141414]/65 p-3 font-bold text-lg"
         >
           Call Now
         </a>
