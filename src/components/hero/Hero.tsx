b034ac4446384789d802bb2e4270d34e18acd98a
orthosquare-marketing <marketing@orthosquare.com>
2025-07-07 08:04:57 +0000

    font size inc

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -23,8 +23,8 @@ const Hero = () => {
               }}
             className="w-full h-12 mt-8 lg:mt-[100px] bg-[#ff7f50] rounded-[5px] flex items-center justify-center gap-2 px-3 sm:px-4 overflow-hidden hover:bg-[#046a9d] transition duration-200">
               <div className="min-w-0 flex items-center gap-2">
-                <span className="whitespace-nowrap overflow-hidden text-ellipsis font-['Poppins'] font-medium text-white text-sm sm:text-base lg:text-[20px] tracking-[-0.5px] leading-normal text-center">
-                Quick Dental Consult at ₹500
+                <span className="whitespace-nowrap overflow-hidden text-ellipsis font-['Poppins'] font-medium text-white text-sm sm:text-lg lg:text-[20px] tracking-[-0.5px] leading-normal text-center">
+                Quick Dental Consult at ₹500
                 </span>
                 <img
                   className="w-[20px] h-[20px] sm:w-[22px] sm:h-[22px] flex-shrink-0"
--- src/components/locationOfClinics/Clinics.tsx
+++ src/components/locationOfClinics/Clinics.tsx
@@ -50,7 +50,7 @@ const Clinics = () => {
               }}
               className="mt-8 bg-[#ff7f50] rounded-[5px] flex items-center justify-between px-5 mx-auto hover:bg-[#046a9d] transition duration-200">
               <span className="whitespace-nowrap overflow-hidden text-ellipsis font-['Poppins'] font-medium text-white text-sm sm:text-base lg:text-[20px] tracking-[-0.5px] leading-normal text-center p-3">
-              Expert Video Consultation with Dr. Shet
+              Expert Video Consultation with Dr. Shet at ₹500
               </span>
               <img
                 className="w-[20px] h-[20px] ml-2"
