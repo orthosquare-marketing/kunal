11c8faa82eec136abe86d412b6f10d600ddc9a91
orthosquare-marketing <marketing@orthosquare.com>
2025-10-02 11:20:04 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Prathamesh } from "./screens/Prathamesh";
+import ThankYou from "./components/ThankYou";
 
 createRoot(document.getElementById("app") as HTMLElement).render(
   <StrictMode>
-    <Prathamesh />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Prathamesh />} />
+        <Route path="/thank-you" element={<ThankYou />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 );
